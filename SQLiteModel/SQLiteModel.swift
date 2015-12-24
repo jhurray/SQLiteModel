5970d4d9e102ee9f410e6d268ff3bf90177ec538
Jeff Hurray <jhurray@umich.edu>
2015-12-24 17:13:22 -0500

    first commit

---
+++ SQLiteModel/SQLiteModel.swift
@@ -0,0 +1,11 @@
+//
+//  SQLiteModel.swift
+//  SQLiteModel
+//
+//  Created by Jeff Hurray on 12/24/15.
+//  Copyright © 2015 jhurray. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
